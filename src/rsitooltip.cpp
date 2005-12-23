e5bcb4df9d39ec3685a2c18e74ca8a76c801717c
Bram Schoenmakers <bramschoenmakers@kde.nl>
2005-12-23 21:07:19 +0000

    And add these files too, of course.
    svn path=/trunk/playground/utils/rsibreak/; revision=490952

---
+++ src/rsitooltip.cpp
@@ -0,0 +1,101 @@
+/* This file is part of the KDE project
+   Copyright (C) 2005 Bram Schoenmakers <bramschoenmakers@kde.nl>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <qlabel.h>
+#include <qpixmap.h>
+#include <qvbox.h>
+
+#include <kdebug.h>
+#include <klocale.h>
+
+#include <math.h>
+
+#include "rsitooltip.h"
+
+RSIToolTip::RSIToolTip( QWidget *parent, const char *name )
+  : KPassivePopup( parent, name )
+{
+  kdDebug() << "RSIToolTip::RSIToolTip() entered" << endl;
+  setTimeout( 10 * 1000 );
+
+  QHBox *hbox = new QHBox( this );
+  hbox->setSpacing( 10 );
+
+  mIcon = new QLabel( hbox );
+  mIcon->resize( 32, 32 );
+
+  QVBox *vbox = new QVBox( hbox );
+  new QLabel( "<qt><strong>RSIBreak</strong></qt>", vbox );
+  mTimeLeft = new QLabel( vbox );
+  mInterval = new QLabel( vbox );
+
+  setView( hbox );
+}
+
+RSIToolTip::~RSIToolTip()
+{
+  kdDebug() << "RSIToolTip::~RSIToolTip() entered" << endl;
+}
+
+void RSIToolTip::setCounters( const QTime &time, const int currentBreak )
+{
+    int s = (int)ceil(QTime::currentTime().msecsTo( time )/1000);
+
+    if (s > 0)
+    {
+        int minutes = (int)floor(s/60);
+        int seconds  = s-(minutes*60);
+        QString mString = i18n("One minute","%n minutes",minutes);
+        QString sString = i18n("One second","%n seconds",seconds);
+        QString finalString;
+
+        if (minutes > 0 && seconds > 0)
+        {
+            mTimeLeft->setText( i18n("First argument: minutes, second: seconds "
+                               "both as you defined earlier",
+                               "%1 and %2 remaining").arg(mString, sString) );
+        }
+        else if ( minutes == 0 && seconds > 0 )
+        {
+            mTimeLeft->setText( i18n("Argument: seconds part or minutes part as "
+                               "defined earlier",
+                               "%1 remaining").arg(sString) );
+        }
+        else if ( minutes >0 && seconds == 0 )
+        {
+            mTimeLeft->setText( i18n("Argument: seconds part or minutes part as "
+                               "defined earlier",
+                               "%1 remaining").arg(mString) );
+        }
+
+        if (currentBreak - 1 == 0)
+            mInterval->setText( i18n("Next break is a big break") );
+        else
+            mInterval->setText( "\n" + i18n("Big break after next break",
+                             "Big break after %n breaks", currentBreak - 1) );
+    }
+}
+
+void RSIToolTip::setPixmap( const QPixmap &pix )
+{
+  kdDebug() << "RSIToolTip::setPixmap() entered" << endl;
+
+  mIcon->setPixmap( pix );
+}
+
+#include "rsitooltip.moc"
---
+++ src/rsitooltip.h
@@ -0,0 +1,51 @@
+/* This file is part of the KDE project
+   Copyright (C) 2005 Bram Schoenmakers <bramschoenmakers@kde.nl>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#ifndef RSITOOLTIP_H
+#define RSITOOLTIP_H
+
+#include <qdatetime.h>
+#include <qimage.h>
+
+#include <kpassivepopup.h>
+
+/**
+ * @class RSIToolTip
+ * This frame is shown as a tooltip, when the user hovers the
+ * mouse above the docker's icon.
+ * @author Bram Schoenmakers <bramschoenmakers@kde.nl>
+ */
+class RSIToolTip : public KPassivePopup
+{
+  Q_OBJECT
+  public:
+    RSIToolTip( QWidget *parent = 0, const char *name = 0 );
+    ~RSIToolTip();
+
+  public slots:
+    void setCounters( const QTime &, const int );
+    void setPixmap( const QPixmap & );
+
+  private:
+    QLabel *mTimeLeft;
+    QLabel *mInterval;
+
+    QLabel *mIcon;
+};
+
+#endif // RSITOOLTIP_H
