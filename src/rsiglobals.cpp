df9586139449ac7d35be257ed3199dcaf70a6f7d
Tom Albers <toma@kde.org>
2006-04-21 17:50:48 +0000

    syntax error before "mins"
    svn path=/trunk/playground/utils/rsibreak/; revision=532336

--- src/rsiglobals.cpp
+++ src/rsiglobals.cpp
@@ -72,11 +72,11 @@ QString RSIGlobals::formatSeconds( const int seconds )
     sString1 = i18n("One second","%n seconds", secs);
     sString2 = i18n("one second","%n seconds", secs);
 
-    if ( hours > 0 and mins >0 )
+    if ( hours > 0 && mins >0 )
         return(i18n("Arguments: hours, minutes "
                     "both as you defined earlier",
                     "%1 and %2").arg(hString, mString2) );
-    else if ( hours > 0 and mins == 0 )
+    else if ( hours > 0 && mins == 0 )
         return( hString );
 
     else if ( hours == 0 )
