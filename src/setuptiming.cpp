27f4ef9cb64ca747fdaa423fb6da4c615b4b4223
Danial Klimkin <dklimkin@google.com>
2017-09-29 20:25:11 +0200

    Adding pause skip threshold.
    Step 2: Actually the feature I wanted. I'd like auto-skip timeout
    to be different (and generally longer) than break time. Defaults
    skip too many tiny breaks just because I am reading docs / emails.

--- src/setuptiming.cpp
+++ src/setuptiming.cpp
@@ -39,14 +39,15 @@ class SetupTimingPriv
 public:
     KPluralHandlingSpinBox*          tinyInterval;
     KPluralHandlingSpinBox*          tinyDuration;
+    KPluralHandlingSpinBox*          tinyThreshold;
     KPluralHandlingSpinBox*          bigInterval;
     KPluralHandlingSpinBox*          bigDuration;
+    KPluralHandlingSpinBox*          bigThreshold;
     KPluralHandlingSpinBox*          postponeDuration;
     int                    debug;
 };
 
-SetupTiming::SetupTiming( QWidget* parent )
-        : QWidget( parent )
+SetupTiming::SetupTiming( QWidget* parent ) : QWidget( parent )
 {
     d = new SetupTimingPriv;
 
@@ -57,34 +58,48 @@ SetupTiming::SetupTiming( QWidget* parent )
     QGroupBox *tinyBox = new QGroupBox( this );
     tinyBox->setTitle( i18n( "Tiny Breaks" ) );
 
-    QWidget *m = new QWidget( this );
-    QHBoxLayout *mHBoxLayout = new QHBoxLayout(m);
-    mHBoxLayout->setMargin(0);
-    QLabel *l1 = new QLabel( i18n( "Short break every:" ) + ' ', m );
-    mHBoxLayout->addWidget(l1);
-    l1->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
-    l1->setWhatsThis( i18n( "Here you can set how often you want a short break. " ) );
-    d->tinyInterval = new KPluralHandlingSpinBox( m );
-    mHBoxLayout->addWidget(d->tinyInterval);
+    QWidget *m1_1 = new QWidget( this );
+    QHBoxLayout *mHBoxLayout = new QHBoxLayout( m1_1 );
+    mHBoxLayout->setMargin( 0 );
+    QLabel *l1_1 = new QLabel( i18n( "Short break every:" ) + ' ', m1_1 );
+    mHBoxLayout->addWidget( l1_1 );
+    l1_1->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
+    l1_1->setWhatsThis( i18n( "Here you can set how often you want a short break. " ) );
+    d->tinyInterval = new KPluralHandlingSpinBox( m1_1 );
+    mHBoxLayout->addWidget( d->tinyInterval );
     d->tinyInterval->setRange( 1, 1000 );
-    l1->setBuddy( d->tinyInterval );
-    connect(d->tinyInterval, static_cast<void (KPluralHandlingSpinBox::*)(int)>(&KPluralHandlingSpinBox::valueChanged), this, &SetupTiming::slotTinyValueChanged);
+    l1_1->setBuddy( d->tinyInterval );
+    connect( d->tinyInterval, static_cast<void ( KPluralHandlingSpinBox::* )( int )>( &KPluralHandlingSpinBox::valueChanged ), this, &SetupTiming::slotTinyValueChanged );
 
-    QWidget *m2 = new QWidget( this );
-    QHBoxLayout *m2HBoxLayout = new QHBoxLayout(m2);
-    m2HBoxLayout->setMargin(0);
-    QLabel *l2 = new QLabel( i18n( "For a duration of:" ) + ' ', m2 );
-    m2HBoxLayout->addWidget(l2);
-    l2->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
-    l2->setWhatsThis( i18n( "Here you can set the duration of the short break." ) );
-    d->tinyDuration = new KPluralHandlingSpinBox( m2 );
-    m2HBoxLayout->addWidget(d->tinyDuration);
+    QWidget *m1_2 = new QWidget( this );
+    QHBoxLayout *m1_2HBoxLayout = new QHBoxLayout( m1_2 );
+    m1_2HBoxLayout->setMargin( 0 );
+    QLabel *l1_2 = new QLabel( i18n( "For a duration of:" ) + ' ', m1_2 );
+    m1_2HBoxLayout->addWidget( l1_2 );
+    l1_2->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
+    l1_2->setWhatsThis( i18n( "Here you can set the duration of the short break." ) );
+    d->tinyDuration = new KPluralHandlingSpinBox( m1_2 );
+    m1_2HBoxLayout->addWidget( d->tinyDuration );
     d->tinyDuration->setRange( 1, 1000 );
-    l2->setBuddy( d->tinyDuration );
+    l1_2->setBuddy( d->tinyDuration );
+
+    QWidget *m1_3 = new QWidget( this );
+    QHBoxLayout *m1_3HBoxLayout = new QHBoxLayout( m1_3 );
+    m1_3HBoxLayout->setMargin( 0 );
+    QLabel *l1_3 = new QLabel( i18n( "Skip if no activity for:" ) + ' ', m1_3 );
+    m1_3HBoxLayout->addWidget( l1_3 );
+    l1_3->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
+    l1_3->setWhatsThis( i18n( "Here you can set the duration of the short break." ) );
+    d->tinyThreshold = new KPluralHandlingSpinBox( m1_3 );
+    m1_3HBoxLayout->addWidget( d->tinyThreshold );
+    d->tinyThreshold->setRange( 1, 1000 );
+    l1_3->setBuddy( d->tinyThreshold );
+
 
     QVBoxLayout *vbox0 = new QVBoxLayout( tinyBox );
-    vbox0->addWidget( m );
-    vbox0->addWidget( m2 );
+    vbox0->addWidget( m1_1 );
+    vbox0->addWidget( m1_2 );
+    vbox0->addWidget( m1_3 );
     vbox0->addStretch( 1 );
     tinyBox->setLayout( vbox0 );
 
@@ -93,58 +108,71 @@ SetupTiming::SetupTiming( QWidget* parent )
     QGroupBox *bigBox = new QGroupBox( this );
     bigBox->setTitle( i18n( "Big Breaks" ) );
 
-    QWidget *m3 = new QWidget( this );
-    QHBoxLayout *m3HBoxLayout = new QHBoxLayout(m3);
-    m3HBoxLayout->setMargin(0);
-    QLabel *l3 = new QLabel( i18n( "Long break every:" ) + ' ', m3 );
-    m3HBoxLayout->addWidget(l3);
-    l3->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
-    l3->setWhatsThis( i18n( "Here you can set how often you want a long break. " ) );
-    d->bigInterval = new KPluralHandlingSpinBox( m3 );
-    m3HBoxLayout->addWidget(d->bigInterval);
+    QWidget *m2_1 = new QWidget( this );
+    QHBoxLayout *m2_1HBoxLayout = new QHBoxLayout( m2_1 );
+    m2_1HBoxLayout->setMargin( 0 );
+    QLabel *l2_1 = new QLabel( i18n( "Long break every:" ) + ' ', m2_1 );
+    m2_1HBoxLayout->addWidget( l2_1 );
+    l2_1->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
+    l2_1->setWhatsThis( i18n( "Here you can set how often you want a long break. " ) );
+    d->bigInterval = new KPluralHandlingSpinBox( m2_1 );
+    m2_1HBoxLayout->addWidget( d->bigInterval );
     d->bigInterval->setRange( 1, 1000 );
-    l3->setBuddy( d->bigInterval );
+    l2_1->setBuddy( d->bigInterval );
 
-    QWidget *m4 = new QWidget( this );
-    QHBoxLayout *m4HBoxLayout = new QHBoxLayout(m4);
-    m4HBoxLayout->setMargin(0);
-    QLabel *l4 = new QLabel( i18n( "For a duration of:" ) + ' ', m4 );
-    m4HBoxLayout->addWidget(l4);
-    l4->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
-    l4->setWhatsThis( i18n( "Here you can set the duration of the long break." ) );
-    d->bigDuration = new KPluralHandlingSpinBox( m4 );
-    m4HBoxLayout->addWidget(d->bigDuration);
+    QWidget *m2_2 = new QWidget( this );
+    QHBoxLayout *m2_2HBoxLayout = new QHBoxLayout( m2_2 );
+    m2_2HBoxLayout->setMargin( 0 );
+    QLabel *l2_2 = new QLabel( i18n( "For a duration of:" ) + ' ', m2_2 );
+    m2_2HBoxLayout->addWidget( l2_2 );
+    l2_2->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
+    l2_2->setWhatsThis( i18n( "Here you can set the duration of the long break." ) );
+    d->bigDuration = new KPluralHandlingSpinBox( m2_2 );
+    m2_2HBoxLayout->addWidget( d->bigDuration );
     d->bigDuration->setRange( 1, 1000 );
-    l4->setBuddy( d->bigDuration );
+    l2_2->setBuddy( d->bigDuration );
+
+    QWidget *m2_3 = new QWidget( this );
+    QHBoxLayout *m2_3HBoxLayout = new QHBoxLayout( m2_3 );
+    m2_3HBoxLayout->setMargin( 0 );
+    QLabel *l2_3 = new QLabel( i18n( "Skip if no activity for:" ) + ' ', m2_3 );
+    m2_3HBoxLayout->addWidget( l2_3 );
+    l2_3->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
+    l2_3->setWhatsThis( i18n( "Here you can set the duration of the long break." ) );
+    d->bigThreshold = new KPluralHandlingSpinBox( m2_3 );
+    m2_3HBoxLayout->addWidget( d->bigThreshold );
+    d->bigThreshold->setRange( 1, 1000 );
+    l2_3->setBuddy( d->bigThreshold );
 
     QVBoxLayout *vbox1 = new QVBoxLayout( bigBox );
-    vbox1->addWidget( m3 );
-    vbox1->addWidget( m4 );
+    vbox1->addWidget( m2_1 );
+    vbox1->addWidget( m2_2 );
+    vbox1->addWidget( m2_3 );
     vbox1->addStretch( 1 );
     bigBox->setLayout( vbox1 );
 
     // ------------------------ Postpone break
-    
+
     QGroupBox *postponeBox = new QGroupBox( this );
     postponeBox->setTitle( i18n( "Postpone Breaks" ) );
 
     QWidget *m5 = new QWidget( this );
-    QHBoxLayout *m5HBoxLayout = new QHBoxLayout(m5);
-    m5HBoxLayout->setMargin(0);
+    QHBoxLayout *m5HBoxLayout = new QHBoxLayout( m5 );
+    m5HBoxLayout->setMargin( 0 );
     QLabel *l5 = new QLabel( i18n( "For a duration of:" ) + ' ', m5 );
-    m5HBoxLayout->addWidget(l5);
+    m5HBoxLayout->addWidget( l5 );
     l5->setAlignment( Qt::AlignRight | Qt::AlignVCenter );
     l5->setWhatsThis( i18n( "Here you can set for how much time you want to postpone a break." ) );
     d->postponeDuration = new KPluralHandlingSpinBox( m5 );
-    m5HBoxLayout->addWidget(d->postponeDuration);
+    m5HBoxLayout->addWidget( d->postponeDuration );
     d->postponeDuration->setRange( 1, 1000 );
     l5->setBuddy( d->postponeDuration );
 
     QVBoxLayout *vbox2 = new QVBoxLayout( postponeBox );
     vbox2->addWidget( m5 );
     vbox2->addStretch( 1 );
-    postponeBox->setLayout( vbox2 );    
-    
+    postponeBox->setLayout( vbox2 );
+
     l->addWidget( tinyBox );
     l->addWidget( bigBox );
     l->addWidget( postponeBox );
@@ -161,6 +189,9 @@ SetupTiming::SetupTiming( QWidget* parent )
     d->debug ? d->postponeDuration->setSuffix( ki18np( " second", " seconds" ) )
     : d->postponeDuration->setSuffix( ki18np( " minute", " minutes" ) );
 
+    d->tinyThreshold->setSuffix( ki18np( " second", " seconds" ) );
+    d->bigThreshold->setSuffix( ki18np( " minute", " minutes" ) );
+
     slotTinyValueChanged( d->tinyInterval->value() );
 
     // Resize to minimum possible.
@@ -168,6 +199,8 @@ SetupTiming::SetupTiming( QWidget* parent )
     d->bigInterval->setFixedSize( d->tinyInterval->sizeHint() );
     d->tinyDuration->setFixedSize( d->tinyInterval->sizeHint() );
     d->bigDuration->setFixedSize( d->tinyInterval->sizeHint() );
+    d->tinyThreshold->setFixedSize( d->tinyThreshold->sizeHint() );
+    d->bigThreshold->setFixedSize( d->bigThreshold->sizeHint() );
     d->postponeDuration->setFixedSize( d->tinyInterval->sizeHint() );
 }
 
@@ -181,8 +214,10 @@ void SetupTiming::applySettings()
     KConfigGroup config = KSharedConfig::openConfig()->group( "General Settings" );
     config.writeEntry( "TinyInterval", d->tinyInterval->value() );
     config.writeEntry( "TinyDuration", d->tinyDuration->value() );
+    config.writeEntry( "TinyThreshold", d->tinyThreshold->value() );
     config.writeEntry( "BigInterval", d->bigInterval->value() );
     config.writeEntry( "BigDuration", d->bigDuration->value() );
+    config.writeEntry( "BigThreshold", d->bigThreshold->value() );
     config.writeEntry( "PostponeBreakDuration", d->postponeDuration->value() );
     config.sync();
 }
@@ -194,9 +229,13 @@ void SetupTiming::readSettings()
 
     d->tinyInterval->setValue( config.readEntry( "TinyInterval", 10 ) );
     d->tinyDuration->setValue( config.readEntry( "TinyDuration", 20 ) );
+    d->tinyThreshold->setValue( config.readEntry( "TinyThreshold", 40 ) );
+    d->tinyThreshold->setMinimum( d->tinyDuration->value() );
     d->bigInterval->setValue( config.readEntry( "BigInterval", 60 ) );
     d->bigInterval->setMinimum( d->tinyInterval->value() );
     d->bigDuration->setValue( config.readEntry( "BigDuration", 1 ) );
+    d->bigThreshold->setValue( config.readEntry( "BigThreshold", 5 ) );
+    d->bigThreshold->setMinimum( d->bigDuration->value() );
     d->postponeDuration->setValue( config.readEntry( "PostponeBreakDuration", 5 ) );
 }
 
