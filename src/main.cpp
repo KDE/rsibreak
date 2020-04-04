b0310083110d35733f6d991c6bcf5b1867d737fa
Albert Astals Cid <aacid@kde.org>
2020-04-04 16:45:23 +0200

    Increase version number

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ int main( int argc, char *argv[] )
 
     KAboutData aboutData( "rsibreak",
                           i18n( "RSIBreak" ),
-                          "0.12.12",
+                          "0.12.13",
                           i18n( "Try to prevent Repetitive Strain Injury by "
                                  "reminding a user to rest." ),
                           KAboutLicense::GPL,
