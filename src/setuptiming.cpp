129c39ad095f33f6bb28dd0ca429534ce2b38e28
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-07-05 09:15:31 +0200

    Convert license headers to SPDX

--- src/setuptiming.cpp
+++ src/setuptiming.cpp
@@ -1,23 +1,10 @@
 /* ============================================================
- * Copyright (C) 2005-2007,2009-2010 by Tom Albers <toma@kde.org>
- * Copyright (C) 2006 Bram Schoenmakers <bramschoenmakers@kde.nl>
- * Copyright (C) 2010 Juan Luis Baptiste <juan.baptiste@gmail.com>
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * ============================================================ */
+    SPDX-FileCopyrightText: 2005-2007, 2009-2010 Tom Albers <toma@kde.org>
+    SPDX-FileCopyrightText: 2006 Bram Schoenmakers <bramschoenmakers@kde.nl>
+    SPDX-FileCopyrightText: 2010 Juan Luis Baptiste <juan.baptiste@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+    ============================================================*/
 
 // Local includes.
 #include "setuptiming.h"
