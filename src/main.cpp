24d5e44e7ee132c79c6fc7f34011794120f654fe
Nicolas Fella <nicolas.fella@gmx.de>
2024-12-10 19:42:15 +0100

    Drop unneeded code to enable HighDPI scaling
    It's enabled by default in Qt6

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,6 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
     QApplication app(argc, argv);
     app.setQuitOnLastWindowClosed(false);
 
